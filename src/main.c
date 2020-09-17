3b85ddb524449ea2f6bb5e3b821bf6290801ee94
Wesley Ford <wford32@gatech.edu>
2020-09-17 16:09:06 -0400

    Implemented readTemp and a moving avergae to normalize temperature readings

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include "main.h"
 
 #include <stdio.h>
+#include <unistd.h>
 
 struct data {
 	double accel[3];
@@ -15,9 +16,24 @@ int main() {
 	readRegister(WHO_AM_I, &addr, 1);
 	printf("%#02x\n", addr);
 	wakeMPU();
-	resetMPU();
-	double temp = -1;
-	temp = readTemp();
-	printf("Temp is %.02f Celcius\n", temp);
-
+	double tempAvg = 0;
+	int size = 100;
+	double temps[size];
+	for(int i = 0; i < size; i++) {
+		temps[i] = 25;
+	}
+	int i = 0;
+	while(1) {
+		if(i >= size) {i=0;}
+		double instTemp = readTemp();
+		//printf("INSTTEMP: %0.2f, oldTEMP:%.2f, i:%d\n", instTemp, temps[(i-1) % size], i);
+		temps[i] = instTemp;
+		//Easy way first:
+		tempAvg = 0;
+		for(int j = 0; j < size; j++){
+			tempAvg += (temps[j] / size);
+		}
+		i++;
+		printf("Temp is %0.3f\r", tempAvg);
+	}
 }
--- src/mpu.c
+++ src/mpu.c
@@ -31,11 +31,10 @@ void resetMPU() {
 
 double readTemp() {
 	uint8_t rawTemp[2];
-	readRegister(TEMP_OUT_H, &rawTemp[0], 1);
-	readRegister(TEMP_OUT_L, &rawTemp[1], 1);
+	readRegister(TEMP_OUT_H, rawTemp, 2);
 	//Need to negate becasue we read the 2's complement of the value
 	//Debug print
-	printf("Raw reg values read: TEMP_H: %d, TEMP_L: %d\n", rawTemp[0], rawTemp[1]);
-	int16_t combined =-(uint16_t)((rawTemp[0] << 8) | rawTemp[1]);
-	return combined / 340 + 36.53;
+	//printf("Raw reg values read: TEMP_H: %d, TEMP_L: %d\n", rawTemp[0], rawTemp[1]);
+	int16_t combined =(uint16_t)((rawTemp[0] << 8) | rawTemp[1]);
+	return (double) combined / 340 + 36.53;
 }
